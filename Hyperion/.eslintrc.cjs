2f1c1fa9ff86175d0433c69e16f01a1e6f502b50
Chirag Ghosh <cghosh828049@gmail.com>
2023-05-21 01:19:16 +0530

    setup auto linting;

--- Hyperion/.eslintrc.cjs
+++ Hyperion/.eslintrc.cjs
@@ -3,8 +3,25 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'eslint-config-prettier', // Make sure it's always the last config, so it gets the chance to override other configs.
   ],
+  settings: {
+    react: {
+      // Tells eslint-plugin-react to automatically detect the version of React to use.
+      version: 'detect',
+    },
+    // Tells eslint how to resolve imports
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
