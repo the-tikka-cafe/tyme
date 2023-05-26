0955ef7808edf182b8a2e3d8397c3a078499cb99
Chirag Ghosh <cghosh828049@gmail.com>
2023-05-26 21:33:37 +0530

    Add router; Add sass directory structure;

--- Hyperion/src/App.tsx
+++ Hyperion/src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
 function App() {
-  return <div>This website is a work in progress.</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ Hyperion/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div className="home-page">This page is WIP</div>;
+}
+
+export default Home;
